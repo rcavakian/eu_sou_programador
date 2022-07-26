d2fc911de164ef03f001cdcb2b4f0e3ccbe6e9e4
Ricardo Camillo Avakian <ravakian@fapex.org.br>
2022-07-26 11:46:05 -0300

    first commit Primeiro Desafio do curso Polimorfismo Flutter

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.deepPurple[200],
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurpleAccent[100],
+            title: const Center(
+              child: Text('Eu sou programador'),
+            ),
+          ),
+          body: Center(
+            child: SizedBox(
+              height: 600,
+              width: 360,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: const Image(
+                          image: AssetImage('assets/images/bobEsponja.webp')),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: const Image(
+                          image: AssetImage(
+                              'assets/images/patrickProgramador.webp')),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
